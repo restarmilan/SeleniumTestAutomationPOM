a4ccd1d390234f370838e1a46e8d2a43446877e5
restarmilan <restarmilan@gmail.com>
2019-12-06 20:00:29 +0100

    add basic keyword methods

--- src/main/java/keywords/KeywordContainer.java
+++ src/main/java/keywords/KeywordContainer.java
@@ -1,6 +1,7 @@
 package keywords;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class KeywordContainer {
 
@@ -8,5 +9,29 @@ public class KeywordContainer {
 
     public KeywordContainer(WebDriver driver) {
         this.driver = driver;
+    }
+
+    public void navigateTo(String url) {
+        driver.get(url);
+    }
+
+    public void clickToElement(WebElement element) {
+        element.click();
+    }
+
+    public void setElementInput(WebElement element, String input) {
+        element.sendKeys(input);
+    }
+
+    public String getPageUrl() {
+        return driver.getCurrentUrl();
+    }
+
+    public boolean elementIsLocated(WebElement element) {
+        return element.isDisplayed();
+    }
+
+    public String getElementInnerText(WebElement element) {
+        return element.getText();
     }
 }
