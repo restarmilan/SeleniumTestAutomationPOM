297c6248de0f6198802d4a581d85c61fa2fa4cd1
restarmilan <restarmilan@gmail.com>
2019-12-06 22:00:54 +0100

    add parameterized test case for multiple checkbox click validation

--- src/test/java/com/codecool/rmilan/selenium/pages/CheckboxDemoPageTest.java
+++ src/test/java/com/codecool/rmilan/selenium/pages/CheckboxDemoPageTest.java
@@ -1,12 +1,18 @@
-package pages;
+package com.codecool.rmilan.selenium.pages;
 
+
+import com.codecool.rmilan.selenium.utils.Constants;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import utils.Constants;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class CheckboxDemoPageTest {
@@ -33,4 +39,10 @@ class CheckboxDemoPageTest {
         assertTrue(check.checkboxIsClicked());
     }
 
+    @ParameterizedTest
+    @MethodSource("com.codecool.rmilan.selenium.utils.TestParams#provideInputsForCheckboxTest")
+    void checkMultipleCheckboxButtonText(List<String> inputs, String expected) {
+        check.multipleCheckBox(inputs);
+        assertEquals(expected, check.getMultipleCheckBoxButtonText());
+    }
 }
