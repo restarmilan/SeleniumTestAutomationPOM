c5f0ab0328249bda8fe61f7091df4d6ccd95c12d
restarmilan <restarmilan@gmail.com>
2019-12-06 17:43:20 +0100

    add separate class for 'keyword' methods

---
+++ src/main/java/keywords/KeywordContainer.java
@@ -0,0 +1,12 @@
+package keywords;
+
+import org.openqa.selenium.WebDriver;
+
+public class KeywordContainer {
+
+    protected WebDriver driver;
+
+    public KeywordContainer(WebDriver driver) {
+        this.driver = driver;
+    }
+}
