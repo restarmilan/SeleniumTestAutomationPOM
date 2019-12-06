cc77ddbb3f06d670656fa648c4c5ac08e7d5de72
restarmilan <restarmilan@gmail.com>
2019-12-06 20:34:52 +0100

    add Constants final class

---
+++ src/main/java/utils/Constants.java
@@ -0,0 +1,8 @@
+package utils;
+
+public final class Constants {
+
+    public static final int TIMEOUT = 20;
+    public static String CHROMEDRIVER_PATH = "/home/rmilan/codecool/test_automation/selenium/chromedriver.exe";
+    public static String BASE_URL = "https://www.seleniumeasy.com/test/";
+}
